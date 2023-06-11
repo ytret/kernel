544f9916ebae498c321f4c42ef1e8c8dca15514e
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-06-11 23:40:14 +0300

    Initial commit

---
+++ src/entry.s
@@ -0,0 +1,28 @@
+                .set FLAG_ALIGN,        1 << 0
+                .set FLAG_MEMINFO,      1 << 1
+
+                .set MAGIC,             0x1BADB002
+                .set FLAGS,             FLAG_ALIGN | FLAG_MEMINFO
+                .set CHECKSUM,          -(MAGIC + FLAGS)
+
+                .section .multiboot_header
+                .align 4
+                .long   MAGIC
+                .long   FLAGS
+                .long   CHECKSUM
+
+                .section .bss
+                .align  16
+stack_bottom:   .skip   16384
+stack_top:
+
+                .section .text
+                .global entry
+                .type entry, @function
+entry:          mov $stack_top, %esp
+                call main
+
+                cli
+1:              hlt
+                jmp 1b
+                .size entry, . - entry
