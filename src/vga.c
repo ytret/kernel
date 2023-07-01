9a8c903e0a4d2f6ed97a5a2351b1e99d2965d370
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-07-01 22:45:16 +0300

    vga: update cursor position

--- src/vga.c
+++ src/vga.c
@@ -1,12 +1,20 @@
 #include <panic.h>
+#include <port.h>
 #include <vga.h>
 
 #include <stddef.h>
 #include <stdint.h>
 
 #define VGA_MEMORY_ADDR         0xB8000
+
 #define MAX_ROWS                25
 #define MAX_COLS                80
+
+#define PORT_CRTC_ADDR          ((uint16_t) 0x3D4)
+#define PORT_CRTC_DATA          ((uint16_t) 0x3D5)
+
+#define REG_CRTC_CURSOR_LOC_HI  ((uint8_t) 0x0E)
+#define REG_CRTC_CURSOR_LOC_LO  ((uint8_t) 0x0F)
 
 static uint16_t * const gp_vga_memory = (uint16_t *) VGA_MEMORY_ADDR;
 
@@ -15,6 +23,7 @@ static uint8_t g_col;
 
 static void put_char(char ch);
 static void put_char_at(uint8_t row, uint8_t col, char ch);
+static void put_cursor_at(uint8_t row, uint8_t col);
 static void scroll(void);
 
 void
@@ -38,6 +47,8 @@ vga_print_str (char const * p_str)
         put_char(ch);
         p_str++;
     }
+
+    put_cursor_at(g_row, g_col);
 }
 
 void
@@ -47,6 +58,8 @@ vga_print_str_len (char const * p_str, size_t len)
     {
         put_char(p_str[idx]);
     }
+
+    put_cursor_at(g_row, g_col);
 }
 
 static void
@@ -90,6 +103,16 @@ put_char_at (uint8_t row, uint8_t col, char ch)
 
     size_t idx = ((row * MAX_COLS) + col);
     gp_vga_memory[idx] = ((0x0F << 8) | ch);
+}
+
+static void
+put_cursor_at (uint8_t row, uint8_t col)
+{
+    size_t char_idx = (row * MAX_COLS + col);
+    port_outb(PORT_CRTC_ADDR, REG_CRTC_CURSOR_LOC_HI);
+    port_outb(PORT_CRTC_DATA, ((uint8_t) (char_idx >> 8)));
+    port_outb(PORT_CRTC_ADDR, REG_CRTC_CURSOR_LOC_LO);
+    port_outb(PORT_CRTC_DATA, ((uint8_t) char_idx));
 }
 
 static void
