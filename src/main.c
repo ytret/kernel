061197e1b508df30a82806508332081ba647fdb2
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-07-01 18:59:25 +0300

    main.c: use printf()

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
+#include <printf.h>
+#include <vga.h>
+
 #include <stdint.h>
-
-#define VGA_MEMORY_ADDR         0xB8000
-
-static uint16_t * const gp_vga_memory = (uint16_t *) VGA_MEMORY_ADDR;
 
 void
 main (void)
 {
-    gp_vga_memory[0] = 0xA141;
+    vga_clear();
+    printf("Hello, World!");
 
     for (;;)
     {}
