c544df8ece61adead747db3005858b1e21404f5f
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-07-01 18:03:35 +0300

    main.c: print character 'A'

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
+#include <stdint.h>
+
+#define VGA_MEMORY_ADDR         0xB8000
+
+static uint16_t * const gp_vga_memory = (uint16_t *) VGA_MEMORY_ADDR;
+
 void
 main (void)
 {
+    gp_vga_memory[0] = 0xA141;
+
     for (;;)
     {}
 }
