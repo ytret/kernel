53a48c67d2c7fd0dd45cf5b289f06cb7776818ed
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-07-01 22:44:43 +0300

    Add port.h

---
+++ src/port.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdint.h>
+
+static inline void
+port_outb (uint16_t port, uint8_t byte)
+{
+    __asm__ volatile ("outb %0, %1"
+                      : /* no outputs */
+                      : "a" (byte), "Nd" (port)
+                      : "memory");
+}
+
+static inline uint8_t
+port_inb (uint16_t port)
+{
+    uint8_t byte;
+    __asm__ volatile ("inb %1, %0"
+                      : "=a" (byte)
+                      : "Nd" (port)
+                      : "memory");
+    return byte;
+}
