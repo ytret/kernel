db99a0c4d1740a6136216ebc3a6671ca4f8b00f3
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-07-02 15:47:39 +0300

    main: initialize PMM and pass MBI to it

--- src/main.c
+++ src/main.c
@@ -1,15 +1,33 @@
 #include <gdt.h>
 #include <idt.h>
+#include <panic.h>
 #include <pic.h>
 #include <pit.h>
+#include <pmm.h>
 #include <printf.h>
 #include <vga.h>
 
+#include <stdint.h>
+
+#define MULTIBOOT_MAGIC_NUM	0x2BADB002
+
 void
-main (void)
+main (uint32_t magic_num, mbi_t const * p_mbi)
 {
     vga_clear();
     printf("Hello, world!\n");
+
+    if (MULTIBOOT_MAGIC_NUM == magic_num)
+    {
+	printf("Booted by a multiboot-compliant bootloader\n");
+	printf("Multiboot information structure is at %P\n", p_mbi);
+    }
+    else
+    {
+	printf("Magic number: 0x%X, expected: 0x%X\n",
+	       magic_num, MULTIBOOT_MAGIC_NUM);
+	panic("booted by an unknown bootloader");
+    }
 
     gdt_init();
     idt_init();
@@ -19,8 +37,7 @@ main (void)
     pic_set_mask(PIT_IRQ, false);
 
     __asm__ volatile ("sti");
-    printf("Interrupts enabled.\n");
+    printf("Interrupts enabled\n");
 
-    for (;;)
-    {}
+    pmm_init(p_mbi);
 }
