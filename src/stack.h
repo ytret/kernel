6489711bf3f9b4e4c7ded03d09f0fd24f3a6b67b
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-07-09 02:29:15 +0300

    stack.h: code formatting

--- src/stack.h
+++ src/stack.h
@@ -20,5 +20,5 @@ void stack_new(stack_t * p_stack, void * p_bottom, size_t size_bytes);
 void     stack_push(stack_t * p_stack, uint32_t value);
 uint32_t stack_pop(stack_t * p_stack);
 
-bool   stack_is_full(stack_t const * p_stack);
-bool   stack_is_empty(stack_t const * p_stack);
+bool stack_is_full(stack_t const * p_stack);
+bool stack_is_empty(stack_t const * p_stack);
