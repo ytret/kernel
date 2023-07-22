06aefae020c79b4e67c03983d6dd5b953e27de7c
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-07-22 10:52:09 +0300

    mbi: mbi_copy() copies the memory map only if the flag is set

--- src/mbi.c
+++ src/mbi.c
@@ -10,13 +10,16 @@ mbi_copy (mbi_t const * p_src)
     //
     __builtin_memcpy(&g_mbi, p_src, sizeof(mbi_t));
 
-    // Copy the memory map.
-    //
-    uint8_t * p_mmap = alloc(p_src->mmap_length);
-    __builtin_memcpy(p_mmap,
-                     ((void const *) p_src->mmap_addr),
-                     p_src->mmap_length);
-    g_mbi.mmap_addr = ((uint32_t) p_mmap);
+    if (p_src->flags & MBI_FLAG_MMAP)
+    {
+        // Copy the memory map.
+        //
+        uint8_t * p_mmap = alloc(p_src->mmap_length);
+        __builtin_memcpy(p_mmap,
+                         ((void const *) p_src->mmap_addr),
+                         p_src->mmap_length);
+        g_mbi.mmap_addr = ((uint32_t) p_mmap);
+    }
 }
 
 mbi_t const *
