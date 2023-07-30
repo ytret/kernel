4b765b8236a3233831255180414c2c0db1663303
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-07-30 11:00:40 +0300

    run_qemu.sh: create an HD image if it does not exist

--- run_qemu.sh
+++ run_qemu.sh
@@ -2,4 +2,23 @@
 
 set -euxo pipefail
 
-qemu-system-i386 -cdrom kernel.iso -d guest_errors "$@"
+HD_PATH=./hd.img
+HD_SIZE=4M  # must be understandable by qemu-img
+
+if [[ ! -f $HD_PATH ]]; then
+    echo "'$HD_PATH' does not exist or is not a regular file."
+    read -p "Create a new hard disk at '$HD_PATH' of size $HD_SIZE? [Y/n] " -n 1 -r
+    echo
+
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        qemu-img create $HD_PATH $HD_SIZE
+    else
+        exit 0
+    fi
+fi
+
+qemu-system-i386 -cdrom kernel.iso                               \
+                 -drive file=$HD_PATH,format=raw,if=none,id=disk \
+                 -device ahci,id=ahci                            \
+                 -device ide-hd,drive=disk,bus=ahci.0            \
+                 -d guest_errors "$@"
