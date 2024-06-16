683d0ccfee86f01729a319d72e8af3ec1f067949
Yuri Tretyakov <ytretyakov18@gmail.com>
2024-06-16 16:56:29 +0300

    create_iso.sh: use either grub-mkrescue or grub2-mkrescue

--- create_iso.sh
+++ create_iso.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
-cp -v build/kernel.elf isodir/boot/kernel.elf
-cp -v build/user.elf isodir/user.elf
+if ! command grub-mkrescue --version &>/dev/null; then
+    if ! command grub2-mkrescue --version &>/dev/null; then
+        echo "Could not find grub-mkrescue or grub2-mkrescue"
+        exit 1
+    else
+        MKRESCUE=grub2-mkrescue
+    fi
+else
+    MKRESCUE=grub-mkrescue
+fi
+
+cp -v build/kernel isodir/boot/kernel.elf
+# cp -v build/user.elf isodir/user.elf
 
 if [ -e kernel.iso ]; then
     rm -ifv kernel.iso
 fi
 
-grub-mkrescue -o kernel.iso isodir/
+$MKRESCUE -o kernel.iso isodir/
