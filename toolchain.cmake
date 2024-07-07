88e480fb3f6a0ccfa337dad763e0d824899ccbc9
Yuri Tretyakov <ytretyakov18@gmail.com>
2024-07-07 17:12:02 +0300

    Make clangd find the correct include files by using -nostdinc and -isystem

--- toolchain.cmake
+++ toolchain.cmake
@@ -4,14 +4,21 @@ set(CMAKE_SYSTEM_PROCESSOR i686)
 set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "Debug|Release")
 set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
 
-set(CMAKE_C_COMPILER "i686-elf-gcc")
-set(CMAKE_C_FLAGS "-ffreestanding -fdiagnostics-color=always")
+execute_process(
+    COMMAND "${CMAKE_CURRENT_LIST_DIR}/tools/find_gcc_system_includes.sh"
+    OUTPUT_VARIABLE SYSTEM_INCLUDE_DIRECTORY
+)
+
+set(CMAKE_C_COMPILER i686-elf-gcc)
+set(CMAKE_C_FLAGS
+    "-ffreestanding -nostdinc -isystem${SYSTEM_INCLUDE_DIRECTORY} -fdiagnostics-color=always"
+)
 
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp")
 
 set(CMAKE_EXE_LINKER_FLAGS
     "-nostdlib -T${CMAKE_CURRENT_SOURCE_DIR}/src/link.ld"
-    )
+)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER)
