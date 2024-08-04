56b0780d6548a6df5b855db3dd47417184340175
Yuri Tretyakov <ytretyakov18@gmail.com>
2024-08-04 20:12:36 +0300

    entry.s: enable SSE

--- src/entry.s
+++ src/entry.s
@@ -34,15 +34,38 @@ stack_bottom:   .skip   16384
 stack_top:
 
                 .section .text
+
                 .global entry
                 .type   entry, @function
 entry:          mov     $stack_top, %esp
                 push    %ebx
                 push    %eax
+                call    enable_sse
                 call    main
                 add     $8, %esp
-
                 cli
 1:              hlt
                 jmp     1b
                 .size   entry, . - entry
+
+                .global enable_sse
+                .type   enable_sse, @function
+enable_sse:     push    %eax
+
+                mov     %cr0, %eax
+                ## Disable coprocessor emulation.
+                and     $0xFFFFFFFB, %eax       # ~CR0.EM = ~(1 << 2)
+                ## Enable coprocessor emulation.
+                or      $0x2, %eax              # CR0.MP = 1 << 1
+                mov     %eax, %cr0
+
+                mov     %cr4, %eax
+                ## Indicate kernel support for FXSAVE and FXRSTOR.
+                or      $0x200, %eax            # CR4.OSFXSR = 1 << 9
+                ## Indicate kernel support for unmasked SIMD FP exceptions.
+                or      $0x400, %eax            # CR4.OSXMMEXCPT = 1 << 10
+                mov     %eax, %cr4
+
+                pop     %eax
+                ret
+                .size   enable_sse, . - enable_sse
