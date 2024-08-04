e0e2e1af2de68e90cf475ecc2648b158430918ba
Yuri Tretyakov <ytretyakov18@gmail.com>
2024-08-04 21:38:02 +0300

    taskmgr.c: fix typo in comment

--- src/taskmgr.c
+++ src/taskmgr.c
@@ -88,7 +88,7 @@ taskmgr_start_scheduler(__attribute__((noreturn)) void (*p_init_entry)(void)) {
     // Critical section.  It ends when the entry is reached.
     __asm__ volatile("cli");
 
-    // If interrupts were disabled and PIT IRQ happened after the following line
+    // If interrupts were enabled and PIT IRQ happened after the following line
     // and before the entry, some bad things would happen to the stack.
     gb_scheduling = true;
 
