788a2b5f5c1d978be75b98affd1a68c162038ece
Yuri Tretyakov <ytretyakov18@gmail.com>
2024-08-05 03:29:38 +0300

    taskmgr.h: remove declaration of a removed function

--- src/taskmgr.h
+++ src/taskmgr.h
@@ -11,5 +11,4 @@ void taskmgr_schedule(void);
 void taskmgr_new_user_task(uint32_t *p_dir, uint32_t entry);
 void taskmgr_go_usermode(uint32_t entry);
 
-uint32_t taskmgr_running_task_id(void);
 void taskmgr_dump_tasks(void);
