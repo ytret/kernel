a4113b5bbd63d17468e02cae641eec023cebfe08
Yuri Tretyakov <ytretyakov18@gmail.com>
2024-08-07 03:35:25 +0300

    syscall.c: print in one batch

--- src/syscall.c
+++ src/syscall.c
@@ -2,11 +2,13 @@
 #include "syscall.h"
 
 void syscall_dispatch(isr_regs_t *p_regs) {
-    kprintf("Syscall\n");
-    kprintf("p_regs->eax = %u\n", p_regs->eax);
-    kprintf("p_regs->ecx = %u\n", p_regs->ecx);
-    kprintf("p_regs->edx = %u\n", p_regs->edx);
-    kprintf("p_regs->ebx = %u\n", p_regs->ebx);
-    kprintf("p_regs->esi = %u\n", p_regs->esi);
-    kprintf("p_regs->edi = %u\n", p_regs->edi);
+    kprintf("Syscall\n"
+            "p_regs->eax = %u\n"
+            "p_regs->ecx = %u\n"
+            "p_regs->edx = %u\n"
+            "p_regs->ebx = %u\n"
+            "p_regs->esi = %u\n"
+            "p_regs->edi = %u\n",
+            p_regs->eax, p_regs->ecx, p_regs->edx, p_regs->ebx, p_regs->esi,
+            p_regs->edi);
 }
