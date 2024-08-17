92c743a90dc27272a4058fb99b160245eb06ca39
Yuri Tretyakov <ytretyakov18@gmail.com>
2024-08-17 20:37:05 +0300

    syscall.c: print arguments

--- src/syscall.c
+++ src/syscall.c
@@ -11,11 +11,13 @@ void syscall_dispatch(isr_regs_t *p_regs) {
     if (num == SYSCALL_SLEEP_MS) {
         syscall_sleep_ms(arg1);
     } else {
-        kprintf("syscall: unknown num %u\n", num);
+        kprintf("syscall: task %u, unknown num %u\n",
+                taskmgr_running_task()->id, num);
     }
 }
 
 static void syscall_sleep_ms(uint32_t duration_ms) {
-    kprintf("syscall: sleep %u ms\n", duration_ms);
+    kprintf("syscall: task %u, sleep %u ms\n", taskmgr_running_task()->id,
+            duration_ms);
     taskmgr_sleep_ms(duration_ms);
 }
