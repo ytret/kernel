a7b8b97e29d7093a84235685f50be91868df27c9
Yuri Tretyakov <ytretyakov18@gmail.com>
2024-08-18 00:48:40 +0300

    syscall.c: remove kprintf output

--- src/syscall.c
+++ src/syscall.c
@@ -1,4 +1,3 @@
-#include "kprintf.h"
 #include "syscall.h"
 #include "taskmgr.h"
 
@@ -10,14 +9,9 @@ void syscall_dispatch(isr_regs_t *p_regs) {
 
     if (num == SYSCALL_SLEEP_MS) {
         syscall_sleep_ms(arg1);
-    } else {
-        kprintf("syscall: task %u, unknown num %u\n",
-                taskmgr_running_task()->id, num);
     }
 }
 
 static void syscall_sleep_ms(uint32_t duration_ms) {
-    kprintf("syscall: task %u, sleep %u ms\n", taskmgr_running_task()->id,
-            duration_ms);
     taskmgr_sleep_ms(duration_ms);
 }
