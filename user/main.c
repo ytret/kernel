3cf78bff00dfe37502210bc0a83e0a2493c2cc55
Yuri Tretyakov <ytretyakov18@gmail.com>
2024-08-17 15:36:02 +0300

    user/main.c: test SYSCALL_SLEEP_MS

--- user/main.c
+++ user/main.c
@@ -1,5 +1,11 @@
 void main(void) {
-    __asm__ volatile("mov $2, %%eax\n"
+    __asm__ volatile("mov $0, %%eax\n"
+                     "mov $1000, %%ecx\n"
+                     "int $100"
+                     : /* output */
+                     : /* input */
+                     : "eax" /* clobbers */);
+    __asm__ volatile("mov $1, %%eax\n"
                      "int $100"
                      : /* output */
                      : /* input */
