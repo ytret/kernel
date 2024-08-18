d4b90692b3cf412d2dd5aef3caf10bb3faaddbfc
Yuri Tretyakov <ytretyakov18@gmail.com>
2024-08-18 16:25:14 +0300

    Add a syscall for task termination (SYSCALL_EXIT)

--- src/syscall.c
+++ src/syscall.c
@@ -2,6 +2,7 @@
 #include "taskmgr.h"
 
 static void syscall_sleep_ms(uint32_t duration_ms);
+static void syscall_exit(uint32_t exit_code);
 
 void syscall_dispatch(isr_regs_t *p_regs) {
     uint32_t num = p_regs->eax;
@@ -9,9 +10,16 @@ void syscall_dispatch(isr_regs_t *p_regs) {
 
     if (num == SYSCALL_SLEEP_MS) {
         syscall_sleep_ms(arg1);
+    } else if (num == SYSCALL_EXIT) {
+        syscall_exit(arg1);
     }
 }
 
 static void syscall_sleep_ms(uint32_t duration_ms) {
     taskmgr_sleep_ms(duration_ms);
+}
+
+static void syscall_exit(uint32_t exit_code) {
+    (void)exit_code;
+    taskmgr_terminate_task(taskmgr_running_task());
 }
--- src/syscall.h
+++ src/syscall.h
@@ -2,7 +2,8 @@
 
 #include "isrs.h"
 
-#define SYSCALL_INT_NUM 100
+#define SYSCALL_INT_NUM  100
 #define SYSCALL_SLEEP_MS 0
+#define SYSCALL_EXIT     1
 
 void syscall_dispatch(isr_regs_t *p_regs);
