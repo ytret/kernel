2f89f96365ae0363e9d77c5fb37811ade9aee8be
Yuri Tretyakov <ytretyakov18@gmail.com>
2024-08-19 19:10:11 +0300

    assert.h: include panic.h

--- src/assert.h
+++ src/assert.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "panic.h" // IWYU pragma: keep
+
 #define ASSERT(cond)                                                           \
     do {                                                                       \
         if (!(cond)) { panic_silent(); }                                       \
