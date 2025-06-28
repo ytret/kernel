fa5585f880cbd4e1cee0c3affa51d8da41669120
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-06-28 11:37:48 +0300

    Add a prototype block device worker process
    See #7

---
+++ src/disk/blkdev.c
@@ -0,0 +1,70 @@
+/**
+ * @file blkdev.c
+ * Block device worker process.
+ */
+
+#include "assert.h"
+#include "disk/blkdev.h"
+#include "kprintf.h"
+#include "mutex.h"
+#include "panic.h"
+#include "queue.h"
+
+#define blkdevMAX_REQS 32
+
+typedef struct {
+    task_mutex_t lock;
+
+    /**
+     * Request queue.
+     * - Item type: const #blkdev_req_t.
+     * - Max itemss: #blkdevMAX_REQS.
+     */
+    queue_t req_queue;
+} blkdev_ctx_t;
+
+static blkdev_ctx_t g_blkdev_ctx;
+
+void blkdev_init(void) {
+    mutex_init(&g_blkdev_ctx.lock);
+    queue_init(&g_blkdev_ctx.req_queue, blkdevMAX_REQS, sizeof(blkdev_req_t));
+}
+
+bool blkdev_enqueue_req(blkdev_req_t *req) {
+    // FIXME: ideally 'req' is const, but it cannot be const because
+    // queue_write() can't work with const pointers.
+
+    return queue_write(&g_blkdev_ctx.req_queue, req);
+}
+
+[[gnu::noreturn]]
+void blkdev_task_entry(void) {
+    // taskmgr_switch_tasks() requires that task entries enable interrupts.
+    __asm__ volatile("sti");
+
+    for (;;) {
+        blkdev_req_t req;
+        queue_read(&g_blkdev_ctx.req_queue, &req, sizeof(blkdev_req_t));
+
+        // Check the request.
+        if (!req.dev_ctx) {
+            kprintf("blkdev: bad request: dev_ctx = NULL\n");
+            continue;
+        }
+        if (!req.dev_if) {
+            kprintf("blkdev: bad request: dev_if = NULL\n");
+            continue;
+        }
+
+        // Wait for the driver to be ready.
+        // FIXME: currently the AHCI driver busy flag is updated in the idle
+        // check function. Once it uses interrupts, we should use a semaphore
+        // that is increased in the IRQ handler and decreased here.
+        while (req.dev_if->f_is_busy) {}
+
+        req.dev_if->f_submit(&req);
+    }
+
+    kprintf("blkdev_task_entry: reached end\n");
+    panic("unexpected behavior");
+}
---
+++ src/disk/blkdev.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "semaphore.h"
+
+typedef struct blkdev_if blkdev_if_t;
+
+typedef enum {
+    BLKDEV_OP_READ,
+} blkdev_op_t;
+
+typedef struct {
+    blkdev_op_t op;
+    uint64_t lba;
+
+    const void *read_buf;
+    size_t read_size;
+    void *write_buf;
+    size_t write_size;
+
+    void *dev_ctx;            //!< Device driver context for the interface.
+    struct blkdev_if *dev_if; //!< Device driver interface for blkdev.
+
+    void (*cb_done)(semaphore_t *sem_done);
+} blkdev_req_t;
+
+struct blkdev_if {
+    bool (*f_is_busy)(void *ctx);
+    void (*f_submit)(const blkdev_req_t *req);
+};
+
+void blkdev_init(void);
+bool blkdev_enqueue_req(blkdev_req_t *req);
+
+[[gnu::noreturn]]
+void blkdev_task_entry(void);
