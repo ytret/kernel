45b980092184d1fc0b4400d248c5d72f5a6bb211
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-06-29 17:11:10 +0300

    cpu: add cpu_write_msr() and cpu_read_msr()

--- src/cpu.c
+++ src/cpu.c
@@ -1,6 +1,6 @@
 #include "cpu.h"
 
-#define CPU_FLAG_IF    (1 << 9)
+#define CPU_FLAG_IF (1 << 9)
 
 uint32_t cpu_get_flags(void) {
     uint32_t eflags;
@@ -12,4 +12,20 @@ uint32_t cpu_get_flags(void) {
 
 bool cpu_get_int_flag(void) {
     return cpu_get_flags() & CPU_FLAG_IF;
+}
+
+void cpu_write_msr(uint32_t msr, uint64_t val) {
+    const uint32_t hi = val >> 32;
+    const uint32_t lo = val & 0xFFFFFFFF;
+    __asm__ volatile("wrmsr"
+                     : /* no output */
+                     : "a"(lo), "d"(hi), "c"(msr)
+                     : /* no clobber */);
+}
+
+uint64_t cpu_read_msr(uint32_t msr) {
+    uint32_t hi;
+    uint32_t lo;
+    asm volatile("rdmsr" : "=a"(lo), "=d"(hi) : "c"(msr) : /* no clobber */);
+    return ((uint64_t)hi << 32) | (uint64_t)lo;
 }
--- src/cpu.h
+++ src/cpu.h
@@ -3,5 +3,27 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define CPU_MSR_APIC_BASE 0x1B
+
+/**
+ * APIC Base Address MSR register.
+ * Refer to Intel SDM 4, Volume 4, December 2021, Table 2-2 "IA-32 Architectural
+ * MSRs", Register Address 1BH.
+ */
+typedef union [[gnu::packed]] {
+    uint64_t val;
+    struct [[gnu::packed]] {
+        uint64_t reserved1 : 8;
+        uint64_t bsp : 1; //!< Boostrap Processor (BSP) flag.
+        uint64_t reserved2 : 1;
+        uint64_t en_x2_apic : 1; //!< Enable X2 APIC Mode.
+        uint64_t apic_gl_en : 1; //!< APIC Global Enable.
+        uint64_t apic_base : 48; //!< APIC Base Address.
+    } bit;
+} cpu_msr_apic_base_t;
+
 uint32_t cpu_get_flags(void);
 bool cpu_get_int_flag(void);
+
+void cpu_write_msr(uint32_t msr, uint64_t val);
+uint64_t cpu_read_msr(uint32_t msr);
