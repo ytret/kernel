067617af03094e0d646fcff8f189769847fe4f45
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-07-05 15:50:30 +0300

    tools/find_gcc_system_includes.sh: show error if i686-elf-gcc is not in PATH

--- tools/find_gcc_system_includes.sh
+++ tools/find_gcc_system_includes.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 # Finds i686-elf-gcc system include directories.
 
-GCC_PATH=$(type i686-elf-gcc | sed 's/^i686-elf-gcc is //')
+if ! i686-elf-gcc --version >/dev/null 2>&1; then
+  echo "i686-elf-gcc not found in PATH" 1>&2
+  echo "script's PATH value: $PATH" 1>&2
+  exit 1
+fi
+
+GCC_PATH=$(type -P i686-elf-gcc)
 GCC_LOCATION=$(dirname "$GCC_PATH")
 GCC_VERSION=$(i686-elf-gcc --version | head -1 | awk '{print $3}')
 
-realpath -L "$GCC_LOCATION/../lib/gcc/i686-elf/$GCC_VERSION/include" |
-    tr -d '\n'
+realpath -L "$GCC_LOCATION/../lib/gcc/i686-elf/$GCC_VERSION/include"
