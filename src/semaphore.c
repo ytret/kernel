5ea48cbe6926d9cea2ee0f99913cde1a8005b0f2
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-07-06 09:10:02 +0300

    semaphore.c: use consts

--- src/semaphore.c
+++ src/semaphore.c
@@ -13,19 +13,19 @@ void semaphore_init(semaphore_t *p_sem) {
 void semaphore_increase(semaphore_t *p_sem) {
     atomic_fetch_add(&p_sem->count, 1);
 
-    list_node_t *p_waiting_task_node = list_pop_first(&p_sem->waiting_tasks);
-    if (p_waiting_task_node) {
-        task_t *p_waiting_task =
-            LIST_NODE_TO_STRUCT(p_waiting_task_node, task_t, list_node);
-        taskmgr_unblock(p_waiting_task);
+    list_node_t *const waiting_node = list_pop_first(&p_sem->waiting_tasks);
+    if (waiting_node) {
+        task_t *const waiting_task =
+            LIST_NODE_TO_STRUCT(waiting_node, task_t, list_node);
+        taskmgr_unblock(waiting_task);
     }
 }
 
 void semaphore_decrease(semaphore_t *p_sem) {
     for (;;) {
-        int old_count = atomic_load(&p_sem->count);
+        const int old_count = atomic_load(&p_sem->count);
         if (old_count > 0) {
-            int new_count = old_count - 1;
+            const int new_count = old_count - 1;
             if (atomic_compare_exchange_weak(&p_sem->count, &old_count,
                                              new_count)) {
                 break;
