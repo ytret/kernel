6265b409b82ff42c073b92ca2dbb91a1a1e927c0
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-07-19 10:07:05 +0300

    tests/fakes: add fake string_equals() and string_dup()

--- tests/fakes/kstring.c
+++ tests/fakes/kstring.c
@@ -2,6 +2,14 @@
 
 #include "kstring.h"
 
+bool string_equals(char const *p_a, char const *p_b) {
+    return strcmp(p_a, p_b) == 0;
+}
+
 size_t string_len(char const *p_str) {
     return strlen(p_str);
+}
+
+char *string_dup(char const *p_str) {
+    return strdup(p_str);
 }
--- tests/fakes/kstring.h
+++ tests/fakes/kstring.h
@@ -6,7 +6,9 @@
 extern "C" {
 #endif
 
+bool string_equals(char const *p_a, char const *p_b);
 size_t string_len(char const *p_str);
+char *string_dup(char const *p_str);
 
 #ifdef __cplusplus
 }
