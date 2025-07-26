5cbbc028287a21a7fdead1fb62084babaff31271
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-07-26 07:27:08 +0300

    tools/loop_hd.sh: use functions from bash/ scripts

--- tools/loop_hd.sh
+++ tools/loop_hd.sh
@@ -1,59 +1,37 @@
 #!/usr/bin/env bash
 # Creates a loopback device for a QEMU disk image.
-#
-# To disable color output, set the environment variable NOCOLOR to something,
-# for example:
-#
-#   NOCOLOR=1 ./loop_hd.sh
 
-if [[ -z "$NOCOLOR" ]]; then
-    CINFO="\033[0;32m"
-    CERR="\033[0;31m"
-    CNONE="\033[0m"
-else
-    CINFO=""
-    CERR=""
-    CNONE=""
-fi
-function e_info {
-    echo -e "${CINFO}$1${CNONE}"
-}
-function e_err {
-    echo -e "${CERR}$1${CNONE}" 1>&2
-}
+set -euo pipefail
+shopt -s inherit_errexit
 
-TOOLS_DIR="$(dirname $(readlink --canonicalize $0))"
-ROOT_DIR="$(readlink --canonicalize "$TOOLS_DIR/..")"
+source "$(dirname $0)/bash/prelude.sh"
 
 if [[ "$#" -eq "1" ]]; then
-    HD_IMG="$1"
+    IMG_FILE="$1"
 else
-    HD_IMG="$ROOT_DIR/hd.img"
+    IMG_FILE="$REPO_DIR/hd.img"
 fi
 
-if [[ ! -f "$HD_IMG" ]]; then
-    e_err "Error: '$HD_IMG' does not exist or is not a regular file"
+if [[ ! -f "$IMG_FILE" ]]; then
+    log_err "Error: '$IMG_FILE' does not exist or is not a regular file"
     exit 1
 fi
 
-e_info "Creating a loopback device for $HD_IMG"
-LOOPN=$(sudo losetup --partscan --find --show "$HD_IMG")
+log_info "Creating a loopback device for $IMG_FILE"
+LOOPN=$(sudo losetup --partscan --find --show "$IMG_FILE")
 
 if [[ -z "$LOOPN" ]]; then
-    e_err "Error: failed to create a loopback device"
+    log_err "Error: failed to create a loopback device"
     exit 1
 fi
 
-e_info "Created $LOOPN."
+log_info "Created $LOOPN."
 
-echo
-e_info "Block devices:"
+log_info -e "\nBlock devices:"
 lsblk $LOOPN
 
-echo
-e_info "Partition table:"
+log_info -e "\nPartition table:"
 sudo fdisk -l $LOOPN
 
-echo
-e_info "Use the following to detach the created loop device:"
-e_info "sudo losetup -d $LOOPN"
+log_info "Use the following to detach the created loop device:"
+log_info "sudo losetup -d $LOOPN"
