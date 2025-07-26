88330dd5629cb726071be110cd654cd4824bb354
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-07-26 07:34:25 +0300

    loop_hd.sh: add -h|--help

--- tools/loop_hd.sh
+++ tools/loop_hd.sh
@@ -6,14 +6,41 @@ shopt -s inherit_errexit
 
 source "$(dirname $0)/bash/prelude.sh"
 
-if [[ "$#" -eq "1" ]]; then
-    IMG_FILE="$1"
+declare -r DEFAULT_IMG_FILE="$REPO_DIR/hd.img"
+
+declare -a POSARGS=()
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            echo "Usage: $0 [options] [FILE]"
+            echo
+            echo "Creates a loopback device (e.g., /dev/loop0) with partition scan for the FILE"
+            echo "disk image (default: '$DEFAULT_IMG_FILE')"
+            echo
+            echo "Options:"
+            echo "    -h, --help"
+            echo "        Print this message and exit."
+            exit 0
+            ;;
+
+        *)
+            POSARGS+=("$1")
+            shift
+            ;;
+    esac
+done
+
+if [[ ${#POSARGS[@]} -gt 1 ]]; then
+    log_err "Unexpected positional arguments: ${POSARGS[@]:1}"
+    exit 1
+elif [[ ${#POSARGS[@]} -eq 1 ]]; then
+    IMG_FILE="$(readlink --canonicalize -- "${POSARGS[0]}")"
 else
     IMG_FILE="$REPO_DIR/hd.img"
 fi
 
 if [[ ! -f "$IMG_FILE" ]]; then
-    log_err "Error: '$IMG_FILE' does not exist or is not a regular file"
+    log_err "Error: image '$IMG_FILE' does not exist or is not a regular file"
     exit 1
 fi
 
