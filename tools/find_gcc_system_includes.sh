a9fb99bc9994d18f528dfe1a43c1a2ff1a0cc5a9
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-07-26 07:52:24 +0300

    tools: find_gcc_system_includes.sh: check for awk; use readlink

--- tools/find_gcc_system_includes.sh
+++ tools/find_gcc_system_includes.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 # Finds i686-elf-gcc system include directories.
 
+set -euo pipefail
+shopt -s inherit_errexit
+
 if ! i686-elf-gcc --version >/dev/null 2>&1; then
-  echo "i686-elf-gcc not found in PATH" 1>&2
-  echo "script's PATH value: $PATH" 1>&2
-  exit 1
+    echo "Error: i686-elf-gcc not found in PATH" 1>&2
+    echo "Error: script's PATH value: $PATH" 1>&2
+    exit 1
+fi
+
+if ! awk --version >/dev/null 2>&1; then
+    echo "Error: awk not found in PATH" 1>&2
+    echo "Error: script's PATH value: $PATH" 1>&2
+    exit 1
 fi
 
 GCC_PATH=$(type -P i686-elf-gcc)
-GCC_LOCATION=$(dirname "$GCC_PATH")
+GCC_DIR=$(dirname -- "$GCC_PATH")
 GCC_VERSION=$(i686-elf-gcc --version | head -1 | awk '{print $3}')
 
-realpath -L "$GCC_LOCATION/../lib/gcc/i686-elf/$GCC_VERSION/include"
+readlink --canonicalize "$GCC_DIR/../lib/gcc/i686-elf/$GCC_VERSION/include"
