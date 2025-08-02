025e6a313b2062dcc050e64cd9ecbe7846cfa3a1
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-08-02 14:21:22 +0300

    tests: add fake kprintf

---
+++ tests/fakes/kprintf.c
@@ -0,0 +1,6 @@
+#include "kprintf.h"
+
+void kprintf(char const *restrict p_format, ...) {
+    (void)p_format;
+    // Do nothing.
+}
---
+++ tests/fakes/kprintf.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void kprintf(char const *restrict p_format, ...);
