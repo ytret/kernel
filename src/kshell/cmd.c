672f93dae0e6bd206bad9e702b96a7302d7f5459
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-08-03 14:03:10 +0300

    kshell/cmd.c: use an array of commands instead of if-else'ing them

--- src/kshell/cmd.c
+++ src/kshell/cmd.c
@@ -5,6 +5,7 @@
 
 #include <cpuid.h>
 
+#include "assert.h"
 #include "kprintf.h"
 #include "kshell/cmd.h"
 #include "kshell/cmd/ksh_devmgr.h"
@@ -13,6 +14,22 @@
 #include "kshell/cmd/ksh_vasview.h"
 #include "kshell/kshscan.h"
 #include "kstring.h"
+
+typedef struct {
+    const char *name;
+    void (*f_handler)(list_t *arg_list);
+} kshell_cmd_t;
+
+static const kshell_cmd_t g_kshell_cmds[] = {
+    // clang-format off
+    {"devmgr", ksh_devmgr},
+    {"mbi", ksh_mbi},
+    {"taskmgr", ksh_taskmgr},
+    {"vasview", ksh_vasview},
+    // clang-format on
+};
+
+static const kshell_cmd_t *prv_kshell_cmd_find(const char *name);
 
 void kshell_cmd_parse(const char *p_cmd) {
     list_t arg_list;
@@ -34,17 +51,22 @@ void kshell_cmd_parse(const char *p_cmd) {
     const char *const arg0_str =
         LIST_NODE_TO_STRUCT(arg0_node, kshscan_arg_t, list_node)->arg_str;
 
-    if (string_equals(arg0_str, "devmgr")) {
-        ksh_devmgr(&arg_list);
-    } else if (string_equals(arg0_str, "mbi")) {
-        ksh_mbi(&arg_list);
-    } else if (string_equals(arg0_str, "taskmgr")) {
-        ksh_taskmgr(&arg_list);
-    } else if (string_equals(arg0_str, "vasview")) {
-        ksh_vasview(&arg_list);
+    const kshell_cmd_t *const cmd = prv_kshell_cmd_find(arg0_str);
+    if (cmd) {
+        ASSERT(cmd->f_handler);
+        cmd->f_handler(&arg_list);
     } else {
         kprintf("kshell: unrecognized command '%s'\n", arg0_str);
     }
 
     kshscan_free_arg_list(&arg_list);
+}
+
+static const kshell_cmd_t *prv_kshell_cmd_find(const char *name) {
+    const size_t num_cmds = sizeof(g_kshell_cmds) / sizeof(g_kshell_cmds[0]);
+    for (size_t idx = 0; idx < num_cmds; idx++) {
+        const kshell_cmd_t *const cmd = &g_kshell_cmds[idx];
+        if (string_equals(name, cmd->name)) { return cmd; }
+    }
+    return NULL;
 }
