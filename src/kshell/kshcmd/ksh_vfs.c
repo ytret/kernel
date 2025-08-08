e6a690fbe2cbd93be6b6ba53ad63e101020051ad
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-08-08 10:29:43 +0300

    kshell: vfs: add action 'mkdir'

--- src/kshell/kshcmd/ksh_vfs.c
+++ src/kshell/kshcmd/ksh_vfs.c
@@ -40,6 +40,7 @@ static const ksharg_parser_desc_t g_ksh_vfs_parser = {
 };
 
 static void prv_ksh_vfs_ls(const char *path);
+static void prv_ksh_vfs_mkdir(const char *path);
 static bool prv_ksh_vfs_resolve_path(const char *path, vfs_node_t **out_node);
 
 void ksh_vfs(list_t *arg_list) {
@@ -99,6 +100,8 @@ void ksh_vfs(list_t *arg_list) {
 
     if (string_equals(action_str, "ls")) {
         prv_ksh_vfs_ls(path_str);
+    } else if (string_equals(action_str, "mkdir")) {
+        prv_ksh_vfs_mkdir(path_str);
     } else {
         kprintf("ksh_vfs: unrecognized action '%s'\n", action_str);
     }
@@ -115,9 +118,8 @@ static void prv_ksh_vfs_ls(const char *path) {
         return;
     }
     if (!node->ops->f_readdir) {
-        kprintf(
-            "ksh_vfs: node at path '%s' does not support action 'readdir'\n",
-            path);
+        kprintf("ksh_vfs: node at path '%s' does not support op 'readdir'\n",
+                path);
         return;
     }
 
@@ -130,7 +132,7 @@ static void prv_ksh_vfs_ls(const char *path) {
     vfs_err_t err = f_readdir(node, dirents, max_dirents, &read_dirents);
 
     if (err != VFS_ERR_NONE) {
-        kprintf("ksh_vfs: op readdir returned error code %u: %s\n", err,
+        kprintf("ksh_vfs: op 'readdir' returned error code %u: %s\n", err,
                 vfs_err_str(err));
         return;
     }
@@ -145,6 +147,64 @@ static void prv_ksh_vfs_ls(const char *path) {
                 "length needs to be increased\n",
                 max_dirents);
     }
+}
+
+static void prv_ksh_vfs_mkdir(const char *path_str) {
+    vfs_err_t err;
+    vfs_path_t path;
+    err = vfs_path_from_str(path_str, &path);
+    if (err != VFS_ERR_NONE) {
+        kprintf(
+            "ksh_vfs: failed to convert '%s' to a path object, error %u: %s\n",
+            path_str, err, vfs_err_str(err));
+        vfs_path_free(&path);
+        return;
+    }
+
+    if (list_is_empty(&path.parts)) {
+        kprintf("ksh_vfs: path '%s' has too few parts\n", path_str);
+        return;
+    }
+    const list_node_t *const last_part_node = list_pop_last(&path.parts);
+    const vfs_path_part_t *const last_part =
+        LIST_NODE_TO_STRUCT(last_part_node, vfs_path_part_t, list_node);
+    const char *const last_part_name = last_part->name;
+
+    vfs_node_t *vfs_node;
+    err = vfs_resolve_path(&path, &vfs_node);
+    if (err != VFS_ERR_NONE) {
+        kprintf("ksh_vfs: failed to resolve '%s' without its last part, error "
+                "%u: %s\n",
+                path_str, err, vfs_err_str(err));
+        vfs_path_free(&path);
+        return;
+    }
+
+    if (!vfs_node->ops) {
+        kprintf("ksh_vfs: node at path '%s' has no ops\n", path_str);
+        vfs_path_free(&path);
+        return;
+    }
+    if (!vfs_node->ops->f_mknode) {
+        kprintf("ksh_vfs: node at path '%s' does not support op 'mknode'\n",
+                path_str);
+        vfs_path_free(&path);
+        return;
+    }
+
+    vfs_node_t *child_node;
+    auto f_mknode = vfs_node->ops->f_mknode;
+    err = f_mknode(vfs_node, &child_node, last_part_name, VFS_NODE_DIR);
+    if (err != VFS_ERR_NONE) {
+        kprintf("ksh_vfs: op 'mknode' returned error code %u: %s\n", err,
+                vfs_err_str(err));
+        vfs_path_free(&path);
+        return;
+    }
+
+    kprintf("ksh_vfs: created directory node at '%s'\n", path_str);
+
+    vfs_path_free(&path);
 }
 
 static bool prv_ksh_vfs_resolve_path(const char *path, vfs_node_t **out_node) {
