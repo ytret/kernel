10a972a60da90c210148d0104d505c13702775bb
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-04-12 15:47:04 +0300

    pci.c: replace kprintf calls with LOG_*

--- src/pci.c
+++ src/pci.c
@@ -7,7 +7,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include "kprintf.h"
+#include "log.h"
 #include "panic.h"
 #include "pci.h"
 #include "port.h"
@@ -66,43 +66,43 @@ const pci_dev_t *pci_get_dev_const(size_t idx) {
 
 void pci_dump_dev_short(const pci_dev_t *dev) {
     if (!dev) { panic("pci_dump_dev_short: dev = NULL"); }
-    kprintf("pci: %u-%u-%u: %04x:%04x class %02x.%02x.%02x\n", dev->bus_num,
-            dev->dev_num, dev->fun_num, dev->header.common.vendor_id,
-            dev->header.common.device_id, dev->header.common.base_class,
-            dev->header.common.sub_class, dev->header.common.interface);
+    LOG_DEBUG("%u-%u-%u: %04x:%04x class %02x.%02x.%02x", dev->bus_num,
+              dev->dev_num, dev->fun_num, dev->header.common.vendor_id,
+              dev->header.common.device_id, dev->header.common.base_class,
+              dev->header.common.sub_class, dev->header.common.interface);
 }
 
 void pci_dump_dev_header(const pci_dev_t *dev) {
     if (!dev) { panic("pci_dump_dev_short: dev = NULL"); }
 
-    kprintf("pci: vendor_id = 0x%04X\n", dev->header.common.vendor_id);
-    kprintf("pci: device_id = 0x%04X\n", dev->header.common.device_id);
-    kprintf("pci: command = 0x%04X\n", dev->header.common.command);
-    kprintf("pci: status = 0x%04X\n", dev->header.common.status);
-    kprintf("pci: revision_id = 0x%02X\n", dev->header.common.revision_id);
-    kprintf("pci: base_class = 0x%02X\n", dev->header.common.base_class);
-    kprintf("pci: sub_class = 0x%02X\n", dev->header.common.sub_class);
-    kprintf("pci: interface = 0x%02X\n", dev->header.common.interface);
-    kprintf("pci: cacheline_size = 0x%02X\n", dev->header.common.cacheline_size);
-    kprintf("pci: latency_timer = 0x%02X\n", dev->header.common.latency_timer);
-    kprintf("pci: header_type = 0x%02X\n", dev->header.common.header_type);
-    kprintf("pci: bist = 0x%02X\n", dev->header.common.bist);
-    kprintf("pci: bar0 = 0x%08X\n", dev->header.bar0);
-    kprintf("pci: bar1 = 0x%08X\n", dev->header.bar1);
-    kprintf("pci: bar2 = 0x%08X\n", dev->header.bar2);
-    kprintf("pci: bar3 = 0x%08X\n", dev->header.bar3);
-    kprintf("pci: bar4 = 0x%08X\n", dev->header.bar4);
-    kprintf("pci: bar5 = 0x%08X\n", dev->header.bar5);
-    kprintf("pci: cardbus_cis_ptr = 0x%08X\n", dev->header.cardbus_cis_ptr);
-    kprintf("pci: subsys_vendor_id = 0x%04X\n", dev->header.subsys_vendor_id);
-    kprintf("pci: subsys_id = 0x%04X\n", dev->header.subsys_id);
-    kprintf("pci: expansion_rom_base_addr = 0x%08X\n",
-            dev->header.expansion_rom_base_addr);
-    kprintf("pci: cap_ptr = 0x%02X\n", dev->header.cap_ptr);
-    kprintf("pci: int_line = 0x%02X\n", dev->header.int_line);
-    kprintf("pci: int_pin = 0x%02X\n", dev->header.int_pin);
-    kprintf("pci: min_gnt = 0x%02X\n", dev->header.min_gnt);
-    kprintf("pci: max_lat = 0x%02X\n", dev->header.max_lat);
+    LOG_DEBUG("vendor_id = 0x%04X", dev->header.common.vendor_id);
+    LOG_DEBUG("device_id = 0x%04X", dev->header.common.device_id);
+    LOG_DEBUG("command = 0x%04X", dev->header.common.command);
+    LOG_DEBUG("status = 0x%04X", dev->header.common.status);
+    LOG_DEBUG("revision_id = 0x%02X", dev->header.common.revision_id);
+    LOG_DEBUG("base_class = 0x%02X", dev->header.common.base_class);
+    LOG_DEBUG("sub_class = 0x%02X", dev->header.common.sub_class);
+    LOG_DEBUG("interface = 0x%02X", dev->header.common.interface);
+    LOG_DEBUG("cacheline_size = 0x%02X", dev->header.common.cacheline_size);
+    LOG_DEBUG("latency_timer = 0x%02X", dev->header.common.latency_timer);
+    LOG_DEBUG("header_type = 0x%02X", dev->header.common.header_type);
+    LOG_DEBUG("bist = 0x%02X", dev->header.common.bist);
+    LOG_DEBUG("bar0 = 0x%08X", dev->header.bar0);
+    LOG_DEBUG("bar1 = 0x%08X", dev->header.bar1);
+    LOG_DEBUG("bar2 = 0x%08X", dev->header.bar2);
+    LOG_DEBUG("bar3 = 0x%08X", dev->header.bar3);
+    LOG_DEBUG("bar4 = 0x%08X", dev->header.bar4);
+    LOG_DEBUG("bar5 = 0x%08X", dev->header.bar5);
+    LOG_DEBUG("cardbus_cis_ptr = 0x%08X", dev->header.cardbus_cis_ptr);
+    LOG_DEBUG("subsys_vendor_id = 0x%04X", dev->header.subsys_vendor_id);
+    LOG_DEBUG("subsys_id = 0x%04X", dev->header.subsys_id);
+    LOG_DEBUG("expansion_rom_base_addr = 0x%08X",
+              dev->header.expansion_rom_base_addr);
+    LOG_DEBUG("cap_ptr = 0x%02X", dev->header.cap_ptr);
+    LOG_DEBUG("int_line = 0x%02X", dev->header.int_line);
+    LOG_DEBUG("int_pin = 0x%02X", dev->header.int_pin);
+    LOG_DEBUG("min_gnt = 0x%02X", dev->header.min_gnt);
+    LOG_DEBUG("max_lat = 0x%02X", dev->header.max_lat);
 }
 
 /**
@@ -114,9 +114,8 @@ static void prv_pci_enumerate_bus(uint8_t bus_num) {
     for (uint8_t dev_num = 0; dev_num < PCI_DEVS_PER_BUS; dev_num++) {
         for (uint8_t fun_num = 0; fun_num < PCI_FUNS_PER_DEV; fun_num++) {
             if (g_pci_num_devs == PCI_MAX_DEVS) {
-                kprintf(
-                    "pci: maximum number of devices (%u) has been reached\n",
-                    PCI_MAX_DEVS);
+                LOG_ERROR("maximum number of devices (%u) has been reached",
+                          PCI_MAX_DEVS);
                 break;
             }
             pci_dev_t *const dev = &g_pci_devs[g_pci_num_devs];
@@ -135,8 +134,8 @@ static void prv_pci_enumerate_bus(uint8_t bus_num) {
 
             if (header.vendor_id == 0xFFFF) { continue; }
             if (header.header_type != 0x00) {
-                kprintf("pci: %u-%u-%u: unknown header type 0x%02x\n", bus_num,
-                        dev_num, fun_num, header.header_type);
+                LOG_ERROR("%u-%u-%u: unknown header type 0x%02x", bus_num,
+                          dev_num, fun_num, header.header_type);
                 continue;
             }
 
