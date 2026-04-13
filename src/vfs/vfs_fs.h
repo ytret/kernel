36293c67584420f73e09190adb572442013e09ae
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-04-13 21:14:18 +0300

    vfs_fs.h: add docs

--- src/vfs/vfs_fs.h
+++ src/vfs/vfs_fs.h
@@ -3,8 +3,37 @@
 #include "vfs/vfs_err.h"
 #include "vfs/vfs_node.h"
 
+/**
+ * Filesystem descriptor.
+ *
+ * Each filesystem has only one struct of this type.
+ */
 typedef struct {
+    /**
+     * ASCII filesystem name.
+     *
+     * Ideally without spaces to remind of a variable, like "ramfs", "ext4",
+     * etc.
+     */
     const char *name;
+
+    /**
+     * Mount the filesystem @a ctx at the VFS node @a node.
+     *
+     * @param ctx  Filesystem context pointer.
+     * @param node VFS node to mount the filesystem at.
+     *
+     * @returns An error code, see #vfs_err_t.
+     */
     vfs_err_t (*f_mount)(void *ctx, vfs_node_t *node);
+
+    /**
+     * Unmount the filesystem @a ctx from the VFS node @a node.
+     *
+     * @param ctx  Filesystem context pointer.
+     * @param node VFS node, which the filesystem is currently mounted at.
+     *
+     * @returns An error code, see #vfs_err_t.
+     */
     vfs_err_t (*f_unmount)(void *ctx, vfs_node_t *node);
 } vfs_fs_desc_t;
