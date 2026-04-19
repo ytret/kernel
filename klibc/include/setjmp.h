a74efe278b7397c45564d5045bfdd1bf00247ff8
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-04-19 14:46:50 +0300

    klibc: setjmp.h: implement setjmp/longjmp in assembly
    Do not use builtins

--- klibc/include/setjmp.h
+++ klibc/include/setjmp.h
@@ -1,6 +1,15 @@
 #pragma once
 
-typedef void *jmp_buf[5];
+#include <stdint.h>
+
+typedef struct {
+    uint32_t ebx;
+    uint32_t esi;
+    uint32_t edi;
+    uint32_t ebp;
+    uint32_t esp;
+    uint32_t eip;
+} jmp_buf[1];
 
 int setjmp(jmp_buf env);
 [[noreturn]] void longjmp(jmp_buf env, int val);
--- klibc/setjmp.s
+++ klibc/setjmp.s
@@ -1,6 +1,40 @@
-                .global setjmp_impl
-                .type   setjmp_impl, @function
-setjmp_impl:    mov     4(%esp), %ecx
-                mov     %ebp, 0(%ecx)
-                mov     %esi, 4(%ecx)
-                .size   setjmp_impl, . - setjmp_impl
+                ## int setjmp(jmp_buf env)
+                .global setjmp
+                .type   setjmp, @function
+setjmp:         mov     4(%esp), %eax   # eax = env
+
+                mov     %ebx, 0(%eax)
+                mov     %esi, 4(%eax)
+                mov     %edi, 8(%eax)
+                mov     %ebp, 12(%eax)
+
+                lea     4(%esp), %edx   # future esp after return
+                mov     %edx, 16(%eax)
+
+                mov     0(%esp), %edx   # return address (eip)
+                mov     %edx, 20(%eax)
+
+                xor     %eax, %eax
+                ret
+                .size   setjmp, . - setjmp
+
+                ## void longjmp(jmp_buf env, int val)
+                .global longjmp
+                .type   longjmp, @function
+longjmp:        mov     4(%esp), %eax   # eax = env
+                mov     8(%esp), %edx   # edx = val
+
+                mov     0(%eax), %ebx
+                mov     4(%eax), %esi
+                mov     8(%eax), %edi
+                mov     12(%eax), %ebp
+
+                mov     16(%eax), %esp
+                mov     20(%eax), %ecx  # ecx = target eip
+
+                test    %edx, %edx
+                jnz     .val_ok
+                mov     $1, %edx        # replace val 0 with 1
+.val_ok:        mov     %edx, %eax      # set return value for setjmp
+                jmp     *%ecx
+                .size   longjmp, . - longjmp
