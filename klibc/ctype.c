89a7a59b303e97d8da6a446de5683a16b40efad7
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-04-19 13:27:51 +0300

    klibc: add stubs for ctype.h

---
+++ klibc/ctype.c
@@ -0,0 +1,50 @@
+#include <ctype.h>
+#include <ytkernel/panic.h>
+
+int isalnum(int c) {
+    PANIC("stub %s called", __func__);
+}
+
+int isalpha(int c) {
+    PANIC("stub %s called", __func__);
+}
+
+int iscntrl(int c) {
+    PANIC("stub %s called", __func__);
+}
+
+int isdigit(int c) {
+    PANIC("stub %s called", __func__);
+}
+
+int isgraph(int c) {
+    PANIC("stub %s called", __func__);
+}
+
+int islower(int c) {
+    PANIC("stub %s called", __func__);
+}
+
+int ispunct(int c) {
+    PANIC("stub %s called", __func__);
+}
+
+int isspace(int c) {
+    PANIC("stub %s called", __func__);
+}
+
+int isupper(int c) {
+    PANIC("stub %s called", __func__);
+}
+
+int isxdigit(int c) {
+    PANIC("stub %s called", __func__);
+}
+
+int toupper(int c) {
+    PANIC("stub %s called", __func__);
+}
+
+int tolower(int c) {
+    PANIC("stub %s called", __func__);
+}
