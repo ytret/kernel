6344ca7ada5fef9f285c8960eb16fb93ea11ba55
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-04-25 22:27:25 +0300

    [tools] Focus QEMU window on macOS
    It is not focused by default

--- tools/run_qemu.sh
+++ tools/run_qemu.sh
@@ -8,6 +8,7 @@ source "$(dirname "$(readlink -f "$0")")/bash/prelude.sh"
 ISO_PATH="$REPO_DIR/build/kernel.iso"
 HD_PATH="$REPO_DIR/hd.img"
 HD_SIZE=4M  # must be understandable by qemu-img
+QEMU_BIN=qemu-system-i386
 
 if [[ ! -f $HD_PATH ]]; then
     log_err "Error: '$HD_PATH' does not exist or is not a regular file"
@@ -16,11 +17,28 @@ if [[ ! -f $HD_PATH ]]; then
     exit 1
 fi
 
-qemu-system-i386 -cdrom "$ISO_PATH"                                \
-                 -drive file="$HD_PATH",format=raw,if=none,id=disk \
-                 -device ahci,id=ahci                              \
-                 -device ide-hd,drive=disk,bus=ahci.0              \
-                 -boot order=d                                     \
-                 -serial stdio                                     \
-                 -d guest_errors                                   \
-                 "$@"
+function macos_focus_qemu() {
+    (
+        sleep 0.3
+
+        local qemu_pid="$(pgrep -n -x "$QEMU_BIN" || true)"
+        [[ -n $qemu_pid ]] || return 0
+
+        osascript >/dev/null 2>&1 <<EOF || true
+tell application "System Events"
+    set frontmost of first application process whose unix id is $qemu_pid to true
+end tell
+EOF
+    ) &
+}
+
+[[ $OSTYPE == darwin* ]] && macos_focus_qemu
+
+"$QEMU_BIN" -cdrom "$ISO_PATH"                                \
+            -drive file="$HD_PATH",format=raw,if=none,id=disk \
+            -device ahci,id=ahci                              \
+            -device ide-hd,drive=disk,bus=ahci.0              \
+            -boot order=d                                     \
+            -serial stdio                                     \
+            -d guest_errors                                   \
+            "$@"
