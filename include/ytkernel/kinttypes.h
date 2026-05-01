6b15ff4727ea02a9ab334b569edfc92703ff52c1
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-01 11:07:58 +0300

    [include] kinttypes.h: add PRIuPTR #30

--- include/ytkernel/kinttypes.h
+++ include/ytkernel/kinttypes.h
@@ -4,10 +4,12 @@
 
 #ifdef __clang__
 #define PRIu32  "u"
+#define PRIuPTR "u"
 #define PRIx32  "x"
 #define PRIxPTR "x"
 #else
 #define PRIu32  "lu"
+#define PRIuPTR "lu"
 #define PRIx32  "lx"
 #define PRIxPTR "lx"
 #endif
