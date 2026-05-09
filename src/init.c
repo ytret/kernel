f351fd591e9aa6178b53e9a39199e71f463f394a
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-09 16:04:19 +0300

    [init] Remove file and vnode test code

--- src/init.c
+++ src/init.c
@@ -3,20 +3,15 @@
  * SMP-aware initial tasks implementation.
  */
 
-#include "assert.h"
 #include "blkdev/blkdev.h"
 #include "devmgr.h"
-#include "fs/ramfs.h"
-#include "heap.h"
 #include "init.h"
-#include "kprintf.h"
 #include "kshell/kshell.h"
 #include "log.h"
 #include "panic.h"
 #include "smp.h"
 #include "taskmgr.h"
 #include "term.h"
-#include "vfs/file.h"
 #include "vfs/vnode.h"
 
 #include "arch/x86/apic/lapic.h"
@@ -39,64 +34,6 @@ void init_bsp_task(void) {
     devmgr_init_blkdev_parts();
 
     vnode_root_init();
-
-    vnode_t *const root_node = vnode_root_node();
-    ASSERT(root_node);
-
-    ramfs_ctx_t *ramfs = ramfs_new(1024);
-    ASSERT(ramfs);
-    const fs_desc_t *ramfs_desc = ramfs_get_desc();
-
-    vfs_err_t vfs_err = ramfs_desc->f_mount(ramfs, root_node);
-    if (vfs_err) { PANIC("mount err %d: %s", vfs_err, vfs_err_str(vfs_err)); }
-
-    vnode_t *foo_node = NULL;
-    vfs_err = root_node->ops->f_mknode(root_node, &foo_node, "foo", VNODE_FILE);
-    if (vfs_err) { PANIC("mknode err %d: %s", vfs_err, vfs_err_str(vfs_err)); }
-
-    file_t file = {0};
-    file.flags = FILE_RDONLY;
-    file_err_t err;
-
-    err = file_open_path_str("/foo", &file);
-    if (err != FILE_ERR_NONE) { PANIC("open error %d", err); }
-
-    size_t buf_size = 32;
-    uint8_t *buf = heap_alloc(buf_size);
-    for (size_t idx = 0; idx < buf_size; idx++) {
-        buf[idx] = 2 * idx;
-    }
-
-    size_t write_size = 16;
-    size_t num_written = 0;
-    err = file_write(&file, buf, write_size, &num_written);
-    if (err == FILE_ERR_NONE) {
-        LOG_INFO("num_written = %zu, wanted to write %zu", num_written, write_size);
-    } else {
-        LOG_ERROR("write error %d", err);
-    }
-
-    size_t num_read = 0;
-    err = file_read(&file, buf, buf_size, &num_read);
-    if (err == FILE_ERR_NONE) {
-        LOG_INFO("num_read = %zu, wanted to read %zu", num_read, buf_size);
-        size_t max_dump_size = 4096;
-        char *dump = heap_alloc(max_dump_size);
-        size_t dump_offset = 0;
-        for (size_t idx = 0; idx < num_read; idx++) {
-            dump_offset +=
-                ksnprintf(&dump[dump_offset], max_dump_size - dump_offset,
-                          "%02x ", ((uint8_t *)buf)[idx]);
-        }
-        LOG_INFO("%s", dump);
-    } else {
-        LOG_ERROR("read error %d", err);
-    }
-
-    LOG_INFO("refcount before file close: %d", foo_node->refcount);
-
-    err = file_close(&file);
-    if (err != FILE_ERR_NONE) { PANIC("close error %d", err); }
 
     kshell();
 
