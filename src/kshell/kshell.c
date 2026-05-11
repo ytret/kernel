175896e347535a9c03cc7657ab71048d1a0ae760
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-11 13:32:31 +0300

    [kshell] Log entering kshell

--- src/kshell/kshell.c
+++ src/kshell/kshell.c
@@ -2,8 +2,11 @@
 #include "kshell/kshcmd/kshcmd.h"
 #include "kshell/kshell.h"
 #include "kshell/kshinput.h"
+#include "log.h"
 
 void kshell(void) {
+    LOG_INFO("enter kshell");
+
     for (;;) {
         kprintf("> ");
         char const *p_cmd = kshinput_line();
