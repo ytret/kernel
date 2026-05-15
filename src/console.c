5a8f5675c77c45daf1b02f768233dce5a7d7646a
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-15 21:31:27 +0300

    Add virtual console module #53

---
+++ src/console.c
@@ -0,0 +1,179 @@
+#include "assert.h"
+#include "console.h"
+#include "heap.h"
+#include "log.h"
+#include "memfun.h"
+#include "pmm.h"
+
+#define CONSOLE_CACHE_CHAR_SIZE 1
+#define CONSOLE_CACHE_ALIGN     PMM_PAGE_SIZE
+
+static void prv_console_scroll(console_t *con);
+
+static void prv_console_redraw_cache(console_t *con);
+static void prv_console_realloc_cache(console_t *con);
+static void prv_console_clear_cache(console_t *con, size_t start_row,
+                                    size_t num_rows);
+
+void console_init(console_t *con) {
+    kmemset(con, 0, sizeof(*con));
+}
+
+bool console_attach(console_t *con, textdisp_t *disp) {
+    DEBUG_ASSERT(con != NULL);
+    DEBUG_ASSERT(disp != NULL);
+
+    if (con->disp) {
+        LOG_ERROR("console %p is already attached to textdisp %p", con,
+                  con->disp);
+        return false;
+    }
+
+    con->rows = textdisp_height(disp);
+    con->cols = textdisp_width(disp);
+    con->disp = disp;
+
+    ASSERT(con->rows != 0);
+    ASSERT(con->cols != 0);
+
+    textdisp_lock(con->disp);
+    prv_console_redraw_cache(con);
+    textdisp_unlock(con->disp);
+
+    return true;
+}
+
+bool console_detach(console_t *con) {
+    DEBUG_ASSERT(con != NULL);
+
+    PANIC("TODO %s", __func__);
+}
+
+void console_clear(console_t *con) {
+    DEBUG_ASSERT(con != NULL);
+
+    prv_console_clear_cache(con, 0, con->rows);
+
+    textdisp_lock(con->disp);
+    textdisp_clear(con->disp);
+    textdisp_unlock(con->disp);
+}
+
+void console_clear_rows(console_t *con, size_t start_row, size_t num_rows) {
+    DEBUG_ASSERT(con != NULL);
+
+    prv_console_clear_cache(con, start_row, num_rows);
+
+    textdisp_lock(con->disp);
+    textdisp_clear_rows(con->disp, start_row, num_rows);
+    textdisp_unlock(con->disp);
+}
+
+void console_put_cursor_at(console_t *con, size_t row, size_t col) {
+    DEBUG_ASSERT(con != NULL);
+    DEBUG_ASSERT(row < con->rows);
+    DEBUG_ASSERT(col < con->cols);
+
+    if (con->cursor_row == row && con->cursor_col == col) { return; }
+
+    con->cursor_row = row;
+    con->cursor_col = col;
+
+    textdisp_lock(con->disp);
+    textdisp_put_cursor_at(con->disp, row, col);
+    textdisp_unlock(con->disp);
+}
+
+void console_put_char_at(console_t *con, size_t row, size_t col, char ch) {
+    DEBUG_ASSERT(con != NULL);
+    DEBUG_ASSERT(row < con->rows);
+    DEBUG_ASSERT(col < con->cols);
+
+    con->cache[col + row * con->cache_row_pitch] = ch;
+
+    textdisp_lock(con->disp);
+    textdisp_put_char_at(con->disp, row, col, ch);
+    textdisp_unlock(con->disp);
+}
+
+void console_put_char(console_t *con, char ch) {
+    DEBUG_ASSERT(con != NULL);
+
+    size_t row = con->cursor_row;
+    size_t col = con->cursor_col;
+
+    switch (ch) {
+    case '\n':
+        col = 0;
+        row++;
+        if (row >= con->rows) {
+            row = con->rows - 1;
+            prv_console_scroll(con);
+        }
+        break;
+
+    default:
+        console_put_char_at(con, row, col, ch);
+
+        col++;
+        if (col >= con->cols) {
+            col = 0;
+            row++;
+            if (row >= con->rows) {
+                row = (con->rows - 1);
+                prv_console_scroll(con);
+            }
+        }
+    }
+
+    console_put_cursor_at(con, row, col);
+}
+
+void console_put_str(console_t *con, const char *str) {
+    DEBUG_ASSERT(con != NULL);
+    DEBUG_ASSERT(str != NULL);
+
+    char ch;
+    while ((ch = *str++)) {
+        console_put_char(con, ch);
+    }
+}
+
+static void prv_console_scroll(console_t *con) {
+    kmemmove(con->cache, &con->cache[con->cache_row_pitch],
+             con->cache_row_pitch * (con->rows - 1));
+
+    textdisp_scroll(con->disp);
+}
+
+static void prv_console_redraw_cache(console_t *con) {
+    prv_console_realloc_cache(con);
+
+    for (size_t row = 0; row < con->rows; row++) {
+        for (size_t col = 0; col < con->cols; col++) {
+            const char ch = con->cache[col + row * con->cache_row_pitch];
+            textdisp_put_char_at(con->disp, row, col, ch);
+        }
+    }
+}
+
+static void prv_console_realloc_cache(console_t *con) {
+    ASSERT(con->rows != 0);
+    ASSERT(con->cols != 0);
+
+    con->cache_row_pitch = con->cols * CONSOLE_CACHE_CHAR_SIZE;
+    con->cache_size = con->rows * con->cache_row_pitch;
+    con->cache = heap_realloc(con->cache, con->cache_size, CONSOLE_CACHE_ALIGN);
+
+    prv_console_clear_cache(con, 0, con->rows);
+}
+
+static void prv_console_clear_cache(console_t *con, size_t start_row,
+                                    size_t num_rows) {
+    ASSERT(start_row < con->rows);
+    ASSERT(num_rows <= con->rows);
+    ASSERT(start_row + num_rows <= con->rows);
+
+    kmemset(&con->cache[start_row * con->cache_row_pitch], ' ',
+            num_rows * con->cache_row_pitch);
+}
---
+++ src/console.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "textdisp.h"
+
+typedef struct {
+    textdisp_t *disp;
+
+    size_t rows;
+    size_t cols;
+
+    char *cache;
+    size_t cache_size;
+    size_t cache_row_pitch;
+
+    size_t cursor_row;
+    size_t cursor_col;
+} console_t;
+
+void console_init(console_t *con);
+
+bool console_attach(console_t *con, textdisp_t *disp);
+bool console_detach(console_t *con);
+
+void console_clear(console_t *con);
+void console_clear_rows(console_t *con, size_t start_row, size_t num_rows);
+
+void console_put_cursor_at(console_t *con, size_t row, size_t col);
+void console_put_char_at(console_t *con, size_t row, size_t col, char ch);
+void console_put_char(console_t *con, char ch);
+void console_put_str(console_t *con, const char *str);
