39cf2823103c67f9dc159b62cb0f034b4fa8346f
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-20 22:38:46 +0300

    [kprintf] Add __attribute-_((format(...))) to kprintf, ksnprintf #49

--- include/ytkernel/kprintf.h
+++ include/ytkernel/kprintf.h
@@ -3,9 +3,11 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-int kprintf(char const *restrict fmt, ...);
+int kprintf(char const *restrict fmt, ...)
+    __attribute__((format(printf, 1, 2)));
 int kvprintf(char const *restrict fmt, va_list ap);
-int ksnprintf(char *str, size_t size, const char *fmt, ...);
+int ksnprintf(char *str, size_t size, const char *fmt, ...)
+    __attribute__((format(printf, 3, 4)));
 int kvsnprintf(char *str, size_t size, const char *fmt, va_list ap);
 int klsnprintf(char *str, size_t size, const char *fmt, ...);
 int kvlsnprintf(char *str, size_t size, const char *fmt, va_list ap);
