24a1dd95dc272fda117266759a1cf34ac88a6296
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-21 22:06:18 +0300

    [include] arch.h: add common arch interface #67

--- include/ytkernel/arch.h
+++ include/ytkernel/arch.h
@@ -1,4 +1,24 @@
 #pragma once
 
-void arch_init_1(void);
-void arch_init_2(void);
+#include <stdint.h>
+
+#include "types.h"
+
+
+void arch_early_init(void);
+void arch_late_init(void);
+void arch_create_platform_tasks(void);
+
+void arch_disable_ints(void);
+void arch_enable_ints(void);
+void arch_get_ints_enabled(void);
+
+void arch_halt_until_int(void);
+void arch_pause_in_loop(void);
+
+void arch_send_ipi(uint8_t proc_num, uint8_t vector);
+void arch_broadcast_ipi(uint8_t vector);
+void arch_ack_ipi(void);
+
+void arch_flush_tlb_addr(vaddr_t addr);
+void arch_flush_tlb(void);
