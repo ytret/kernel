3bcac0c42ba1232ec3006beff3102da8be272ba1
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-24 11:59:40 +0300

    [arch] Move arch-specific macros to include/arch/x86/arch_defs.h #67

--- include/ytkernel/arch.h
+++ include/ytkernel/arch.h
@@ -5,15 +5,11 @@
 #include "smp.h"
 #include "types.h"
 
-#define ARCH_VEC_HALT          0xF1 //!< Halt on panic.
-#define ARCH_VEC_TLB_SHOOTDOWN 0xF2 //!< TLB shootdown.
-
-// physical/virtual (identity-mapped)
-#define ARCH_AP_TRAMPOLINE_ADDR 0x8000
-#define ARCH_AP_TRAMPLINE_ARGS  0x8800
-
-// virtual
-#define ARCH_AP_INIT_STACK_TOP 0xA000 // page 0x9000..0x9FFF
+#ifdef YTKERNEL_ARCH_X86
+#include "arch/x86/arch_defs.h" // IWYU pragma: export
+#else
+#error "Please update include/ytkernel/arch.h to include arch_defs.h"
+#endif
 
 void arch_early_init(void);
 void arch_late_init(void);
