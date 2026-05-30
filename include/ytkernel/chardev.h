71285c16e548fb773fda87f56b456ee00a00261f
Yuri Tretyakov <ytretyakov18@gmail.com>
2026-05-30 10:55:54 +0300

    [chardev] Add op read #55

--- include/ytkernel/chardev.h
+++ include/ytkernel/chardev.h
@@ -8,10 +8,12 @@ typedef enum {
     CHARDEV_UNINIT,
     CHARDEV_SERIAL,
     CHARDEV_CONSOLE,
+    CHARDEV_TTY,
 } chardev_type_t;
 
 typedef struct {
     int (*f_write)(void *ctx, const void *buf, size_t buf_size);
+    int (*f_read)(void *ctx, void *buf, size_t buf_size);
 } chardev_ops_t;
 
 struct chardev {
